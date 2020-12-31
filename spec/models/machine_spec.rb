6acc3cd0d73c7e6091817b0917c492080934cdb4
Isabelle Villasenor <villasenori@outlook.com>
2020-12-31 09:13:20 -0700

    Add Many-To-Many Relationship to Machine Model and Spec

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Machine, type: :model do
+describe Machine do
   describe 'validations' do
-    it { should validate_presence_of :location }
-    it { should belong_to :owner }
+    it { should validate_presence_of(:location) }
+  end
+  
+  describe 'relationships' do
+    it { should belong_to(:owner) }
+    it { should have_many(:machine_snacks) }
+    it { should have_many(:snacks).through(:machine_snacks) }
   end
 end
