b9cad5184ebffe14c16a0ccb9dbdfd37eed120e8
Isabelle Villasenor <villasenori@outlook.com>
2020-12-31 12:02:49 -0700

    Add dependent: :destroy To Models

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,5 +1,5 @@
 class Machine < ApplicationRecord
-  has_many :machine_snacks
+  has_many :machine_snacks, dependent: :destroy
   has_many :snacks, through: :machine_snacks
   belongs_to :owner
   validates_presence_of :location
