91c4f17d4a82a2e5fe3abab8b1305b766ddd4d05
Isabelle Villasenor <villasenori@outlook.com>
2020-12-31 10:27:05 -0700

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Owner.destroy_all
+Machine.destroy_all
+
+o1 = Owner.create!(name: 'Owner 1')
+o2 = Owner.create!(name: 'Owner 2')
+
+m1 = o1.machines.create!(location: 'denver')
+m2 = o2.machines.create!(location: 'utrecht')
+
+takis = Snack.create!(name: 'Takis', price: 1.00)
+beuno = Snack.create!(name: 'Beuno', price: 1.25)
+snv = Snack.create!(name: 'Salt and Vinegar Chips', price: 1.00)
+twix = Snack.create!(name: 'Twix', price: 1.25)
+stroop = Snack.create!(name: 'Stroopwaffel', price: 2.00)
+
+MachineSnack.create!(machine: m1, snack: takis)
+MachineSnack.create!(machine: m1, snack: snv)
+MachineSnack.create!(machine: m1, snack: twix)
+MachineSnack.create!(machine: m2, snack: beuno)
+MachineSnack.create!(machine: m2, snack: stroop)
+MachineSnack.create!(machine: m2, snack: takis)
